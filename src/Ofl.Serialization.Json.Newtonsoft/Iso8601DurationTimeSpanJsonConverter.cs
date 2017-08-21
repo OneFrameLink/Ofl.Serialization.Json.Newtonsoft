5db17445d51baf6e006271135c80cc1961784fe2
casperOne <casperOne@caspershouse.com>
2017-08-20 23:19:29 -0400

    Added JSON converter for ISO 8601 durations.

---
+++ src/Ofl.Serialization.Json.Newtonsoft/Iso8601DurationTimeSpanJsonConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace Ofl.Serialization.Json.Newtonsoft
+{
+    public class Iso8601DurationTimeSpanJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) => objectType == typeof(TimeSpan) || objectType == typeof(TimeSpan?);
+
+        public override bool CanRead => true;
+        public override bool CanWrite => false;
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            // Validate parameters.
+            if (reader == null) throw new ArgumentNullException(nameof(reader));
+            if (objectType != typeof(TimeSpan) && objectType != typeof(TimeSpan?))
+                throw new ArgumentException($"The { nameof(objectType) } parameter must be a { nameof(TimeSpan) } or a { typeof(TimeSpan?).Name }.", nameof(objectType));
+
+            // Get the value.
+            var spanString = reader.Value as string;
+
+            // If the value is null, return null.
+            if (spanString == null) return null;
+
+            // Convert.
+            return XmlConvert.ToTimeSpan(spanString);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            // Validate parameters.
+            if (writer == null) throw new ArgumentNullException(nameof(writer));
+
+            // Get the duration.
+            var duration = (TimeSpan?) value;
+
+            // If null, write null.
+            if (duration == null)
+                // Write null.
+                writer.WriteNull();
+            else
+                // Write the value.
+                writer.WriteValue(XmlConvert.ToString(duration.Value));
+        }
+    }
+}
---
+++ test/Ofl.Serialization.Json.Newtonsoft.Tests/Iso8601DurationTimeSpanJsonConverterTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Ofl.Serialization.Json.Newtonsoft.Tests
+{
+    public class Iso8601DurationTimeSpanJsonConverterTests
+    {
+        private class NonNullableTest
+        {
+            [JsonConverter(typeof(Iso8601DurationTimeSpanJsonConverter))]
+            public TimeSpan Value { get; set; }
+        }
+
+        [Theory]
+        [InlineData("{\"value\":\"PT1H\" }", "01:00:00")]
+        [InlineData("{\"value\":\"PT1M\" }", "00:01:00")]
+        [InlineData("{\"value\":\"PT1S\" }", "00:00:01")]
+        [InlineData("{\"value\":\"PT1H1M\" }", "01:01:00")]
+        [InlineData("{\"value\":\"PT1H1S\" }", "01:00:01")]
+        [InlineData("{\"value\":\"PT1M1S\" }", "00:01:01")]
+        [InlineData("{\"value\":\"PT1H1M1S\" }", "01:01:01")]
+        public void Test_ReadNonNullableJson(string json, string durationString)
+        {
+            // Validate parameters.
+            if (string.IsNullOrWhiteSpace(json)) throw new ArgumentNullException(nameof(json));
+            if (string.IsNullOrWhiteSpace(durationString)) throw new ArgumentNullException(nameof(durationString));
+
+            // Get the expected value.
+            TimeSpan expected = TimeSpan.Parse(durationString);
+
+            // Parse/etc.
+            JObject obj = JObject.Parse(json);
+
+            // Map to an object.
+            NonNullableTest test = obj.ToObject<NonNullableTest>();
+
+            // Equal.
+            Assert.Equal(expected, test.Value);
+        }
+
+        private class NullableTest
+        {
+            [JsonConverter(typeof(Iso8601DurationTimeSpanJsonConverter))]
+            public TimeSpan? Value { get; set; }
+        }
+
+        [Theory]
+        [InlineData("{\"value\": null }", null)]
+        [InlineData("{\"value\":\"PT1H\" }", "01:00:00")]
+        [InlineData("{\"value\":\"PT1M\" }", "00:01:00")]
+        [InlineData("{\"value\":\"PT1S\" }", "00:00:01")]
+        [InlineData("{\"value\":\"PT1H1M\" }", "01:01:00")]
+        [InlineData("{\"value\":\"PT1H1S\" }", "01:00:01")]
+        [InlineData("{\"value\":\"PT1M1S\" }", "00:01:01")]
+        [InlineData("{\"value\":\"PT1H1M1S\" }", "01:01:01")]
+        public void Test_ReadNullableJson(string json, string durationString)
+        {
+            // Validate parameters.
+            if (string.IsNullOrWhiteSpace(json)) throw new ArgumentNullException(nameof(json));
+
+            // Get the expected value.
+            TimeSpan? expected = string.IsNullOrWhiteSpace(durationString) ? 
+                (TimeSpan?) null : TimeSpan.Parse(durationString);
+
+            // Parse/etc.
+            JObject obj = JObject.Parse(json);
+
+            // Map to an object.
+            NullableTest test = obj.ToObject<NullableTest>();
+
+            // Equal.
+            Assert.Equal(expected, test.Value);
+        }
+    }
+}
